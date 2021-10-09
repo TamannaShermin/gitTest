7100fe7679a1cf814a70585a6cfa3327bd21c3d5
AsifUchchas <asifuddinahmed123@gmail.com>
2021-10-09 14:26:57 +0600

    Deleted a line and added one

--- src/class1.java
+++ src/class1.java
@@ -2,7 +2,7 @@ public class class1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world 2");
-        System.out.println("hello world 3");
+        //deleted this line
         System.out.println("Hi There");
         //Added a new line
 
